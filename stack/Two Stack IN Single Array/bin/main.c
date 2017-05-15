fe5b9551069fe7d6e96817a20f6626940d59bc8c
pankaj-joshi123 <pankaj.girish.joshi@gmail.com>
2017-05-15 19:57:21 +0530

    Balanced Parenthesis Theory and Algo

---
+++ stack/Two Stack IN Single Array/bin/main.c
@@ -0,0 +1,12 @@
+in the Geeks for geeks method he has given if closing types of brackets apperars then pop the previous....if the popped element appears then
+it is okay if they are different
+then that means they are not balanced
+
+
+
+********
+
+i have my approach also that i scan the full string and pop two times one at top and one at the bottom at any moment if they are not equal
+then it shows that they are not balanced
+
+but this is more time consuming also more operations are required to it 
